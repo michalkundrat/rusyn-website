c16b5b4a66ad429de5a6a97b1ce0ffc3b82f55d0
ihavezerohealth <michallahcim01@gmail.com>
2022-03-09 20:04:35 +0000

    Tried something, I don't know what.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,42 @@
 <template>
   <v-container fluid class="fill-width fill-height">
-    <v-row justify="center">
+    <v-row justify="center" no-gutters>
       <v-img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Flag_of_Rusyns_2007.svg/640px-Flag_of_Rusyns_2007.svg.png"
-        max-width="320px"
-        max-height="192px"
-        class="mb-n2"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Flag_of_Rusyns_2007.svg/640px-Flag_of_Rusyns_2007.svg.png"
+          max-width="320px"
+          max-height="192px"
+          class="mb-n2"
       ></v-img>
     </v-row>
-    <v-row justify="center">
-      <span class="pb-16 mb-16 px-16 text-center">
-        Thank you for visiting our website! We are committed to improving and
-        allowing you to fulfil your dreams of learning Rusyn! It is a
-        language that is, unfortunately, quickly dying out. It is not yet in
-        danger of being extinct, but fewer and fewer people grow up speaking the
-        language every year.
-      </span>
+    <v-row class="d-flex" justify="center" no-gutters>
+      <v-col class="ml-auto" cols=3 order="first">
+        <v-img
+          src="https://ih1.redbubble.net/image.2340959836.9606/st,small,507x507-pad,600x600,f8f8f8.jpg"
+          width="200px"
+          height="200px"
+          class="mb-n2"
+        ></v-img>
+      </v-col>
+      <v-col class="ml-auto" cols=3>
+        <span class="text-center">
+          Thank you for visiting our website! We are committed to improving and
+          allowing you to fulfil your dreams of learning Rusyn! It is a
+          language that is, unfortunately, quickly dying out. It is not yet in
+          danger of being extinct, but fewer and fewer people grow up speaking the
+          language every year.
+        </span>
+      </v-col>
+      <v-col class="ml-auto" cols=3 order="last">
+        <v-img
+          src="https://ih1.redbubble.net/image.2340959836.9606/st,small,507x507-pad,600x600,f8f8f8.jpg"
+          width="200px"
+          height="200px"
+          class="mb-n2"
+        ></v-img>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio nobis sint, explicabo rem nam ullam natus modi dolor aliquid perspiciatis libero? Nulla sequi dolorem at aut deleniti quod, numquam et? Dicta debitis voluptas quod laboriosam fugit commodi iure iste, voluptate minus, rem corrupti aperiam, tempora reprehenderit modi. Ipsa perspiciatis quam at? Et placeat odit perspiciatis ab beatae autem quo inventore, saepe nam expedita. Repellendus id culpa reiciendis consectetur similique, optio assumenda repellat, aut, sequi possimus ipsum. Assumenda aliquid earum libero!</span>
     </v-row>
   </v-container>
 </template>
