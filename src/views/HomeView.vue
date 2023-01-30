97418e4127223b393e9d010448088b7d88add4bb
ihavezerohealth <michallahcim01@gmail.com>
2023-01-30 20:11:47 +0000

    Changed name and added translation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,10 @@
         <div class="page1">
             <p class="pt-6 text-center text-md-h2">Learn Rusyn</p>
             <p class="text-center text-md-h5">
-                Welcome to Learn Rusyn, the place to learn the Rusyn language.
+                Welcome to LearnRusyn, the place to learn the Rusyn language.
             </p>
             <p class="text-center text-md-h5">
-                translation of the above
+                Вітайте у LearnRusyn, містце де ся навчиете руснацкый язык.
             </p>
         </div>
         <!-- <div class="page2">
@@ -17,16 +17,17 @@
 
 <style>
 .page1 {
-    background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.418), rgba(255, 255, 255, 0)),
+    background-image: linear-gradient(to bottom, rgba(224, 224, 224, 0.7), rgba(255, 255, 255, 0)),
     url(../assets/semetkovce.jpg);
     background-size: cover;
-    height: 93.425vh;
+    height: 100%;
     width: 100%;
+    position: fixed;
 }
 .page2 {
     background-image: url(../assets/rusyn-culture.jpg);
     background-size: cover;
-    height: 100vh;
+    height: 100%;
     width: 100%;
 }
 </style>
